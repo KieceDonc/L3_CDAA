539550080a35a54a0c28f06c47c418043fd0100a
RenardBOT <dinoking.36@gmail.com>
2021-10-20 23:53:32 +0200

    Ajout des squelettes de classes

---
+++ Projet/src/Interaction.cpp
@@ -0,0 +1,26 @@
+#include "../headers/Interaction.h"
+
+
+Interaction::Interaction(const tm&, std::string&) {
+    
+}
+
+Interaction::~Interaction() {
+    
+}
+
+void Interaction::setContent(const std::string&) {
+    
+}
+
+void Interaction::setDate(const tm&) {
+    
+}
+
+std::string Interaction::getContent() {
+    
+}
+
+tm * Interaction::getDate() {
+    
+}
