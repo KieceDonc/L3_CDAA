00f57d2962c5f9c6012fee2854d2aa66873f85e3
RenardBOT <pevan.ers@gmail.com>
2021-10-27 16:44:25 +0200

    Delete Projet/doc/html directory

--- Projet/doc/html/search/all_b.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['_7einteractiontodos_82',['~InteractionTodos',['../class_interaction_todos.html#a8d77dbda7b79795b7d326fb02a745d34',1,'InteractionTodos']]],
-  ['_7elogs_83',['~Logs',['../class_logs.html#a6227976848fb64144ff93587479df43d',1,'Logs']]]
-];
--- Projet/doc/html/search/classes_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['interaction_86',['Interaction',['../class_interaction.html',1,'']]],
-  ['interactiontodo_87',['InteractionTodo',['../class_interaction_todo.html',1,'']]],
-  ['interactiontodos_88',['InteractionTodos',['../class_interaction_todos.html',1,'']]]
-];
